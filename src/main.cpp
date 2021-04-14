d9582970b0b495a99dd0d1c16047e19794505a09
Faces <faceshc@gmail.com>
2021-04-14 20:24:00 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,26 +4,26 @@
 int main()
 {
     int_t Nombre;
-    std::cout << "Entrez le nombre que vous voulez décomposer : ";
+    std::cout << "Entrez le nombre que vous voulez décomposer: ";
     std::cin >> Nombre;
     std::cout << std::endl;
 
     fint a(Nombre);
-    std::cout << "L'écriture en facteurs premiers de a est :" << a << std::endl;
+    std::cout << "Écriture en facteurs premiers de a est: " << a << std::endl;
     std::cout << std::boolalpha;
     std::cout << "a.to_int() : " <<  a.to_int() << std::endl;
     std::cout << "a.is_prime() : " << a.is_prime() << std::endl;
     std::cout << std::endl;
 
-    std::cout << "Entrez le second nombre avec lequel réaliser les calculs : ";
+    std::cout << "Entrez le second nombre avec lequel réaliser les calculs: ";
     std::cin >> Nombre;
     fint b(Nombre);
-    std::cout << "L'écriture en facteurs premiers de b est :" << b << std::endl;
+    std::cout << "Écriture en facteurs premiers de b est: " << b << std::endl;
     std::cout << "b.divides(a) : " << b.divides(a) << std::endl;
     std::cout << std::endl;
     
     std::cout << "============= Operator overloading =============" << std::endl;
-    std::cout << "a == b : " << (a == b) << "a != b : " << (a != b) << std::endl;
+    std::cout << "a == b : " << (a == b) << " & a != b : " << (a != b) << std::endl;
     std::cout << "a * b = " << (a*b) << std::endl;
     std::cout << "a / b = " << (a/b) << std::endl;
     
